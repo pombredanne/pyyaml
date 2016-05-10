5bea998f0623f7e7c933bbbbe6436a0265bc1698
Donald Stufft <donald@stufft.io>
2016-05-10 10:43:27 -0400

    Slight classifiers cleanup

--- setup.py
+++ setup.py
@@ -21,9 +21,11 @@ setuptools.setup(
     url="https://github.com/dstufft/yaml",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
+
         "Intended Audience :: Developers",
+
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.6",
@@ -32,6 +34,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Text Processing :: Markup",
     ],
